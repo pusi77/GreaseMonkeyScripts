a90702be281cd94d046a3c29e8cb5f2ffdfa23fa
pusi77 <pusineriandrea@gmail.com>
2024-06-03 10:54:38 +0200

    added ExamTopicsCirlceRemover

---
+++ ExamTopicsCirlceRemover/ExamTopicsCirlceRemover.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        ExamTopics Cirlce Remover
+// @namespace   https://github.com/pusi77
+// @match       https://www.examtopics.com/exams/*/*/view/
+// @grant       none
+// @version     1.0
+// @author      pusi77
+// @description Removes all "correct answers" (which are never correct) green circle from the page
+// ==/UserScript==
+
+window.addEventListener('load', async function() {
+    console.log("Removing correct (wrong) answers circle...")
+    let listItems = document.querySelectorAll('li');
+
+    listItems.forEach(function(li) {
+        li.classList.remove('correct-hidden');
+        li.classList.remove('correct-choice');
+    });
+  console.log("Removed correct (wrong) answers circle")
+
+  }, false);
+
